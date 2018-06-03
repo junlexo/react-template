c66ce7e1fb09808b9e27c00828d4bfacea47f8ab
Junlexo <thanh55th2@gmail.com>
2018-06-04 00:08:53 +0700

    okie

---
+++ cfg/base.js
@@ -0,0 +1,62 @@
+'use strict';
+let path = require('path');
+let defaultSettings = require('./defaults');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Additional npm or bower modules to include in builds
+// Add all foreign plugins you may need into this array
+// @example:
+// let npmBase = path.join(__dirname, '../node_modules');
+// let additionalPaths = [ path.join(npmBase, 'react-bootstrap') ];
+let additionalPaths = [];
+
+module.exports = {
+  additionalPaths: additionalPaths,
+  port: defaultSettings.port,
+  debug: true,
+  devtool: 'eval',
+  output: {
+    path: path.join(__dirname, '/../dist/assets'),
+    filename: 'app.js',
+    publicPath: defaultSettings.publicPath
+  },
+  devServer: {
+    contentBase: './src/',
+    historyApiFallback: true,
+    hot: true,
+    port: defaultSettings.port,
+    publicPath: defaultSettings.publicPath,
+    noInfo: false
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      actions: `${defaultSettings.srcPath}/actions/`,
+      components: `${defaultSettings.srcPath}/components/`,
+      sources: `${defaultSettings.srcPath}/sources/`,
+      stores: `${defaultSettings.srcPath}/stores/`,
+      styles: `${defaultSettings.srcPath}/styles/`,
+      config: `${defaultSettings.srcPath}/config/` + process.env.REACT_WEBPACK_ENV,
+      'react/lib/ReactMount': 'react-dom/lib/ReactMount'
+    }
+  },
+  module: {
+    rules: [
+      /*
+      your other rules for JavaScript transpiling go in here
+      */
+      { // css / sass / scss loader for webpack
+        test: /\.(css|sass|scss)$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+        })
+      }
+    ]
+  },
+  // plugins: [
+  //   new ExtractTextPlugin({ // define where to save the file
+  //     filename: 'styles/main.css',
+  //     allChunks: true,
+  //   }),
+  // ],
+};
